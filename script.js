105f24fb1c222e906bbf2b7a1fd6da619113d716
RedMalachite <anluvi@ukr.net>
2023-12-09 15:07:43 +0200

    Correct

--- script.js
+++ script.js
@@ -1,49 +1,22 @@
-// let str = prompt('Введите строку:');
-// let reverseStr = '';
-// function reverseString(str) {
-//     for (let i = str.length - 1; i >= 0; i--) {
-//         reverseStr += str[i];
-//     }
-//     return reverseStr;
-// }
-// console.log(reverseString(str));
+const originalArray = [1, 2, 3, 4, 5];
+function reverseArray(originalArray){
+    return originalArray.reverse();
+}
+const reversedArray = reverseArray(originalArray);
+console.log(reversedArray);
 
 
-
-
-
-// let str = prompt('Введите строку:', 'Учуя молоко, я около мяучу');
-// function isPalindrome(str) {
-//     let result = new String();
-//     for (let i = 0; i <= str.length - 1; i++) {
-//         switch (str[i]) { // убираем лишние знаки в предложении
-//             case ' ':
-//             case '.':
-//             case ',':
-//             case '-':
-//                 continue;
-//         }
-//         result += str[i].toLowerCase();
-//     }
-//     let reverseResult = new String();
-//     for (let i = result.length - 1; i >= 0; i--) {
-//         reverseResult += result[i];
-//     }
-//     if (result === reverseResult) return 'Палиндром';
-//     return "Не палиндром";
-// }
-// console.log(isPalindrome(str));
-
-
-
-
-// let firstNum = parseInt(prompt("Введите первое число"));
-// let secondNum = parseInt(prompt("Введите второе число"));
-// function findGCD(firstNum, secondNum) {
-//     let result = new String();
-//     for (let i = 1; i <= Math.min(firstNum, secondNum); i++) {
-//         if (firstNum % i === 0 && secondNum % i === 0) result = i;
-//     }
-//     return result;
-// }
-// console.log(`НОД числел ${firstNum} и ${secondNum} является число ${findGCD(firstNum, secondNum)}`);
+const students = [
+    {name: "Alice", age: 20, grade: 4.5},
+    {name: "Bob", age: 21, grade: 3.9},
+    {name: "Charlie", age: 19, grade: 4.8}
+];
+function calculateAverageGrade(students) {
+    let average = new Number();
+    for (let i = 0; i < students.length; i++) {
+        average += students[i].grade;
+    }
+    average = average / students.length;
+    return average.toFixed(1);
+}
+console.log(calculateAverageGrade(students));
