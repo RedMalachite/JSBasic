ff77a93899cef77071d7610d6406fb7bb67a23b0
RedMalachite <anluvi@ukr.net>
2023-12-11 15:53:08 +0200

    Correct

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-const originalArray = [1, 2, 3, 4, 5];
-function reverseArray(originalArray){
-    return originalArray.reverse();
-}
-const reversedArray = reverseArray(originalArray);
-console.log(reversedArray);
+// const originalArray = [1, 2, 3, 4, 5];
+// const reverseArray = (originalArray) => originalArray.reverse();
+// const reversedArray = reverseArray(originalArray);
+// console.log(reversedArray);
+
+// const array1 = [1, 2, 3, 4, 5];
+// const array2 = [3, 4, 5, 6, 7];
+// const uniqueValues = (arr1, arr2) => [...new Set(arr1.concat(arr2))];
+// const uniqueValuesArray = uniqueValues(array1, array2);
+// console.log(uniqueValuesArray);
+
+// const students = [
+//     {name: "Alice", age: 20, grade: 4.5},
+//     {name: "Bob", age: 21, grade: 3.9},
+//     {name: "Charlie", age: 19, grade: 4.8}
+// ];
+// const averageGrade = students => students.reduce((sum, student) => sum + student.grade, 0) / students.length;
+// const result = averageGrade(students).toFixed(1);
+// console.log(result);
 
 
-const students = [
-    {name: "Alice", age: 20, grade: 4.5},
-    {name: "Bob", age: 21, grade: 3.9},
-    {name: "Charlie", age: 19, grade: 4.8}
-];
-function calculateAverageGrade(students) {
-    let average = new Number();
-    for (let i = 0; i < students.length; i++) {
-        average += students[i].grade;
-    }
-    average = average / students.length;
-    return average.toFixed(1);
-}
-console.log(calculateAverageGrade(students));
 
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-function uniqueValues(array1, array2){
-   let newArray = array1.concat(array2);
-   newArray = [...new Set(newArray)];
-   return newArray;
-}
-const uniqueValuesArray = uniqueValues(array1, array2);
-console.log(uniqueValuesArray);
