ed8c0aae416c8f5099f24a84f8071abfb8591d75
RedMalachite <anluvi@ukr.net>
2023-12-14 21:33:43 +0200

    Correct

--- script.js
+++ script.js
@@ -7,7 +7,9 @@
 // }
 // let result = decor(currentVal);
 
-// let arr = [10, 3, 15, 7, 5, 6, 60];
+
+
+// let arr = [10, 3, 15, 7, 5, 6, 60, 20];
 // let check = (arr) => arr % 5;
 // let decor = (func, validator) => {
 //     for (let i = 0; i < arr.length; i++) {
@@ -24,17 +26,28 @@
 // }
 // let result = decor(check, 0);
 
-let arr = [4, '3', 8, [1, 2], 5, 'string', 110];
-let check = (elem) => typeof (elem);
-let decor = (func, maxAttempts) => {
-    let lastNumber = null;
-    for (let i = 0; i < arr.length; i++) {
-        if (func(arr[i]) == 'number'){
-            lastNumber = arr[i];
-            // console.log(`Значение массива ${arr[i]} - ${func(arr[i])}`);
-        }
-    }
 
-    console.log(lastNumber);
-}
-let result = decor (check, 3);
+
+// // Код перебирает массив в поисках числовых типов данных и логирует сообщение об успешном выполнении.
+// // В случае maxAttempts ошибок логируется error-сообщение и выводится последнее числовое значение.
+// let arr = [4, '3', 8, [1, 2], 5.5, 'string', 110];
+// let check = (elem) => typeof (elem);
+// let decor = (func, maxAttempts) => {
+//     let lastNumber = null;
+//     let attempt = null;
+//     for (let i = 0; i < arr.length; i++) {
+//         try {
+//             if (func(arr[i]) == 'number') {
+//                 lastNumber = arr[i];
+//             } else attempt++;
+//             if (attempt === maxAttempts) {
+//                 throw new Error(`Достигнуто максимальное количество ошибок. Последнее корректное значение: ${lastNumber}`)
+//             }
+//         } catch (error) {
+//             console.log(error);
+//             break;
+//         }
+//     }
+//     if (attempt < maxAttempts) console.log(`Массив перебран без ошибок или с ошибками с количеством меньше позволенного: ${maxAttempts}`);
+// }
+// let result = decor(check, 3);
