60cb27d3cebb7aee8a6794de7633f8feb0a07f9b
RedMalachite <anluvi@ukr.net>
2023-12-14 20:38:21 +0200

    Correct

--- script.js
+++ script.js
@@ -24,3 +24,17 @@
 // }
 // let result = decor(check, 0);
 
+let arr = [4, '3', 8, [1, 2], 5, 'string', 110];
+let check = (elem) => typeof (elem);
+let decor = (func, maxAttempts) => {
+    let lastNumber = null;
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i]) == 'number'){
+            lastNumber = arr[i];
+            // console.log(`Значение массива ${arr[i]} - ${func(arr[i])}`);
+        }
+    }
+
+    console.log(lastNumber);
+}
+let result = decor (check, 3);
