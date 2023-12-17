8d2899b11cf947ecd38e479f41351de02782d2fd
RedMalachite <anluvi@ukr.net>
2023-12-17 14:11:48 +0200

    Correct

--- script.js
+++ script.js
@@ -1,53 +1,8 @@
-// let arr = ['string', true, Infinity, 6, NaN];
-// let currentVal = (value) => value;
-// let decor = (func) => {
-//     for (let i = 0; i < arr.length; i++) {
-//         console.log(func(arr[i]));
+// let userNum1 = parseInt(prompt("Первое число"));
+// let userNum2 = parseInt(prompt("Второе число"));
+// let summarize = (num1) => {
+//     return function (num2) {
+//         num2 || num2 === 0 ? console.log(num1 + num2) : console.log(++num1);
 //     }
 // }
-// let result = decor(currentVal);
-
-
-
-// let arr = [10, 3, 15, 7, 5, 6, 60, 20];
-// let check = (arr) => arr % 5;
-// let decor = (func, validator) => {
-//     for (let i = 0; i < arr.length; i++) {
-//         try {
-//             if (func(arr[i]) === validator) {
-//                 console.log(`Число ${arr[i]} делится на 5 без остатка.`);
-//             } else {
-//                 throw new Error(`Число ${arr[i]} НЕ делится на 5 без остатка.`);
-//             }
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     }
-// }
-// let result = decor(check, 0);
-
-
-
-// // Код перебирает массив в поисках числовых типов данных и логирует сообщение об успешном выполнении.
-// // В случае maxAttempts ошибок логируется error-сообщение и выводится последнее числовое значение.
-// let arr = [4, '3', 8, [1, 2], 5.5, 'string', 110];
-// let check = (elem) => typeof (elem);
-// let decor = (func, maxAttempts) => {
-//     let lastNumber = null;
-//     let attempt = null;
-//     for (let i = 0; i < arr.length; i++) {
-//         try {
-//             if (func(arr[i]) == 'number') {
-//                 lastNumber = arr[i];
-//             } else attempt++;
-//             if (attempt === maxAttempts) {
-//                 throw new Error(`Достигнуто максимальное количество ошибок. Последнее корректное значение: ${lastNumber}`)
-//             }
-//         } catch (error) {
-//             console.log(error);
-//             break;
-//         }
-//     }
-//     console.log(`Массив перебран без ошибок или с ошибками с количеством меньше позволенного: ${maxAttempts}`);
-// }
-// let result = decor(check, 3);
+// summarize(userNum1)(userNum2);
