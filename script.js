436187b214be29a6cd7781c1a56e56a4e967c405
RedMalachite <anluvi@ukr.net>
2024-01-14 16:45:58 +0200

    Correct

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // const randomDelayPrint = () => {
-//     const phrase = prompt('Введите строку:', 'Hello, World!');
+//     const phrase = prompt('Enter a phrase:', 'Hello, World!');
 //     let step = 0;
-//
 //     function printNextSymbol() {
 //         if (step < phrase.length) {
 //             const randomInterval = Math.round(Math.random() * 1000);
@@ -10,40 +9,84 @@
 //             setTimeout(printNextSymbol, randomInterval);
 //         } else return console.log('End');
 //     }
-//
 //     setTimeout(printNextSymbol, Math.round(Math.random() * 1000));
 //     console.log('Start');
 // }
 // randomDelayPrint();
 
 
-let arr = [
-    function plus(a, b) {
-        return console.log(a + b);
-    },
-    function minus(a, b) {
-        return console.log(a - b);
-    },
-    function multiplication(a, b) {
-        return console.log(a * b);
-    },
-    function dividing(a, b) {
-        return console.log(a / b);
-    }
-]
-let timer = 1500;
-const intervalRace = (arr, timer) => {
-    let step = 0;
 
-    const start = () => {
-        if (step < arr.length) {
-            arr[step](6, 4);
-            step++;
-        } else {
-            clearInterval(timer);
-        }
-    };
-    start();
-    setInterval(start, timer);
-}
-intervalRace(arr, timer);
+
+
+// let a = prompt("1-st number", "15");
+// let b = prompt("2-nd number", "5");
+// let arr = [
+//     function plus(a, b) {
+//         console.log(+a + +b);
+//         return +a + +b;
+//     },
+//     function minus(a, b) {
+//         console.log(a - b);
+//         return a - b;
+//     },
+//     function multiplication(a, b) {
+//         console.log(a * b);
+//         return a * b;
+//     },
+//     function dividing(a, b) {
+//         console.log(a / b);
+//         return a / b;
+//     }
+// ];
+// let timer = 1500;
+// const intervalRace = (arr, timer) => {
+//     let step = 0;
+//     let results = [];
+//     const start = () => {
+//         if (step < arr.length) {
+//             results.push(arr[step](a, b));
+//             step++;
+//             setTimeout(start, timer);
+//         } else {
+//             console.log(results);
+//         }
+//     };
+//     start();
+// };
+// intervalRace(arr, timer);
+
+
+
+
+// function debounce(callback, timer) {
+//     let timerId;
+//     let isFirstCall = true;
+//     return function() {
+//         if (isFirstCall) {
+//             callback.apply(this);
+//             isFirstCall = false;
+//         } else {
+//             clearTimeout(timerId);
+//         }
+//
+//         timerId = setTimeout(function() {
+//             isFirstCall = true;
+//         }, timer);
+//     };
+// }
+//
+// function myFunction() {
+//     console.log('Function result');
+// }
+// const debouncedExpensiveOperation = debounce(myFunction, 1000);
+// console.log("1-st call");
+// debouncedExpensiveOperation();
+// console.log("2-nd call");
+// debouncedExpensiveOperation();
+// console.log("3-rd call");
+// debouncedExpensiveOperation();
+// setTimeout(function() {
+//     debouncedExpensiveOperation();
+// }, 1500);
+// console.log("4-th call");
+// debouncedExpensiveOperation();
